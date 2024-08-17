ee3e400b4fa2a6d75bc1eeb04735e7ea07dd7b72
taoshumin_vendor <tsmidx@gmail.com>
2024-08-17 22:13:16 +0800

    bug: 修复files

--- openai/upload.go
+++ openai/upload.go
@@ -26,7 +26,7 @@ import (
 type UploadRequest struct {
 	ConversationId string                `form:"conversation_id"`
 	UploadType     string                `form:"upload_type"`
-	File           *multipart.FileHeader `form:"files"`
+	File           *multipart.FileHeader `form:"file"`
 }
 
 func (r *UploadRequest) Validate() error {
